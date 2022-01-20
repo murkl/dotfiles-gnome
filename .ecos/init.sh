054dad99f6507f18218e7c7836cbda900a057aa7
moritz <moritz@moritz-notebook>
2022-01-20 19:59:58 +0100

    auto_commit_update

--- .ecos/init.sh
+++ .ecos/init.sh
@@ -54,7 +54,8 @@ update() {
 
     # Gnome Minimal -  https://archlinux.org/groups/x86_64/gnome/
     # Mutter Rounded Corners (open settings: mutter_settings)
-    exec_paru "gdm gnome-shell gnome-control-center gnome-terminal nautilus xdg-user-dirs mutter-rounded"
+    exec_paru "mutter-rounded" # Install first to prevent conflict with original mutter
+    exec_paru "gdm gnome-shell gnome-control-center gnome-terminal nautilus xdg-user-dirs"
 
     # Nautilus Addons
     exec_paru "python-nautilus sushi nautilus-sendto nautilus-image-converter"
